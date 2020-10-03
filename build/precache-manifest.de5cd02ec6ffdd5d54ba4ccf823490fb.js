8d2339e615b22293aef282ca74a559b43ea95b66
jianghao <563110789@example.com>
2020-10-04 07:37:16 +0800

    react1

---
+++ build/precache-manifest.de5cd02ec6ffdd5d54ba4ccf823490fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a5213b55d95b149ea49ed339c1299c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd2574f1652b285f5c2b",
+    "url": "/static/js/2.f8e35e53.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f8e35e53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "967c4c5c8b73f94563c4",
+    "url": "/static/js/main.bcaa862b.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
